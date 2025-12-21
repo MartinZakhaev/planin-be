73f566b7412cf616cd09ae1f18e4d80e8567017d
MartinZakhaev <andikawahyusyaputra@gmail.com>
2025-12-22 00:00:20 +0700

    feat(app): prisma setup

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, OnModuleInit } from "@nestjs/common";
+import { PrismaClient } from "src/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from 'pg';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+    constructor() {
+        // 1. Buat Connection Pool PostgreSQL
+        const connectionString = `${process.env.DATABASE_URL}`;
+        const pool = new Pool({ connectionString });
+
+        // 2. Buat Adapter (bedanya di sini, PG butuh instance pool)
+        const adapter = new PrismaPg(pool);
+
+        // 3. Pass adapter ke super() agar PrismaClient menggunakannya
+        super({ adapter });
+    }
+
+    async onModuleInit() {
+        // Tetap panggil $connect() untuk inisialisasi koneksi Prisma saat modul start
+        await this.$connect();
+    }
+}
